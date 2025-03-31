2ed404d1c7d8ef9ddce570527a1d294116f391d3
tduy03 <dinhtuanduy1@gmail.com>
2025-04-01 00:05:38 +0700

    video 16

---
+++ src/components/MyComponent.js
@@ -0,0 +1,17 @@
+// class component
+
+// function component
+import React from "react";
+
+class MyComponent extends React.Component{
+    render() {
+        return (
+            <div>Dinh Tuan Duy
+                {Math.random()}
+            </div>
+
+        );
+    }
+}
+
+export default MyComponent;
