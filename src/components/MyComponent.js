f6961c5a3c59936c447509f8acac24e40ee93822
tduy03 <dinhtuanduy1@gmail.com>
2025-04-01 11:07:52 +0700

    video 19

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -10,10 +10,22 @@ class MyComponent extends React.Component{
         age: 26
     };
 
+    handleClick(event){
+        console.log('clickclap')
+        console.log("My name is ", this.state.name)
+    }
+
+    handleOnMoveOver(event){
+        console.log(event.pageX);
+        
+    }
+
     render() {
         return (
             <div>
                 My name is {this.state.name} and I'm from {this.state.address}
+                <button onClick={this.handleClick}>click me</button>
+                <button onMouseOver={this.handleOnMoveOver}>Hover me</button>
             </div>
 
         );
